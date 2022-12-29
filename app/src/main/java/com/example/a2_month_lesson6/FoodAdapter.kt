eadd382e129c69441f72eda64bc4105239306af7
UrazbaevT <utilek990@gmail.com>
2022-12-29 18:50:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a2_month_lesson6/FoodAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.a2_month_lesson6
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.a2_month_lesson6.databinding.ActivityMainBinding
+import com.example.a2_month_lesson6.databinding.ItemFoodBinding
+
+class FoodAdapter(
+    val foodList: ArrayList<Food>,
+    val onClick: (position: Int) -> Unit
+    ): Adapter<FoodAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemFoodBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind()
+    }
+
+    override fun getItemCount(): Int = foodList.size
+
+
+
+    inner class ViewHolder(private val binding: ItemFoodBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(){
+            val item = foodList[adapterPosition]
+            binding.apply {
+                binding.tvFood.text = item.name
+                binding.imgFood.loadImage(item.picture)
+            }
+
+            itemView.setOnClickListener {
+                onClick(adapterPosition)
+            }
+        }
+    }
+
+
+}
